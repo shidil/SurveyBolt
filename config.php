cea67820673ff03ddb4643fa1989d92ecd70fb54
Shidil <shidil@live.com>
2013-12-25 23:51:18 +0530

    created project skeleton

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+ 
+ /*
+ * 
+ 	Master config file.
+ 	this file contains config informations for app and database.
+ 	@author Shidil Boss
+ 	25-12-2013 11:36 PM
+ *
+ */
+
+$config = array();
+
+///////////////////// Application  ///////////////////////////
+
+define('FROOT', 'c:\server\www\survey');	// File root of the project
+
+define("DOMAIN", "http://localhost/survey/"); /// Domain root
+
+////////////////////   Database   ////////////////////////////
+
+$config['db_host'] = '127.0.0.1';
+$config['db_name'] = 'survey';
+$config['db_user'] = 'bhmg';
+$config['db_pass'] = 'survey_bhmg';
+
+?>

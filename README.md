d13eeb66d4ca68f6f64abc3b15c3980d018756bf
Shidil Dinesh <shidil@live.com>
2014-09-24 01:50:26 +0530

    Add some info

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ SurveyBolt
 =============================
 
 SurveyBolt is an online survey system written in PHP.
+
+Features
+=============================
+	1. Survey automation
+	2. Intutive User Interface
+	3. Easy survey creation
+	4. Effortless Analysis
+	5. Graphs for statistics
+	6. Flexible, Customizable
+
+Configure
+==========
+
+	Edit the config.php
--- index.php
+++ index.php
@@ -19,6 +19,7 @@ Bolt::connect();
 $uSys = new UserSystem;
 $sSys = new SurveySystem;
 
+// these are the rules that rewrites the url.
 
 $rules = array( 
     'login'   => "/login",    // '/picture/some-text/51'
@@ -33,6 +34,7 @@ $uri = rtrim( dirname($_SERVER["SCRIPT_NAME"]), '/' );
 $uri = '/' . trim( str_replace( $uri, '', $_SERVER['REQUEST_URI'] ), '/' );
 $uri = urldecode( $uri );
 
+// code for parsing get arguments
 foreach ( $rules as $action => $rule ) {
     if ( preg_match( '~^'.$rule.'$~i', $uri, $params ) ) {
         /* now you know the action and parameters so you can 
